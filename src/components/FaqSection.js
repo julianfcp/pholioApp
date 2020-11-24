7f4666c377136fd9acb3d29a3580b6d5e603d629
Julian <julian.fcp@gmail.com>
2020-11-24 02:05:36 -0500

    4. Faq Section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,59 @@
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+            architecto consequatur error a beatae debitis reprehenderit,
+            asperiores ullam est, doloremque voluptatem possimus sequi vero qui
+            magnam repellendus! Harum, quam a!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+            architecto consequatur error a beatae debitis reprehenderit,
+            asperiores ullam est, doloremque voluptatem possimus sequi vero qui
+            magnam repellendus! Harum, quam a!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Different Payment Methods</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+            architecto consequatur error a beatae debitis reprehenderit,
+            asperiores ullam est, doloremque voluptatem possimus sequi vero qui
+            magnam repellendus! Harum, quam a!
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>What products do you offer?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+            architecto consequatur error a beatae debitis reprehenderit,
+            asperiores ullam est, doloremque voluptatem possimus sequi vero qui
+            magnam repellendus! Harum, quam a!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
