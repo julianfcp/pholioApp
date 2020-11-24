78acf67d0ccb2e8d5b95a15a0b8dc64e2570a813
Julian <julian.fcp@gmail.com>
2020-11-24 01:58:39 -0500

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,14 @@
 //Page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default AboutUs;
